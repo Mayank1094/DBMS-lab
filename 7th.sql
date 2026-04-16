eba59f739adafefbbbde984e01d10798dcc45edc
Mayank1094 <mayankkamble1094@gmail.com>
2026-04-16 12:33:12 +0530

    Update 7th.sql

--- 7th.sql
+++ 7th.sql
@@ -1,9 +1,9 @@
-insert into employee22 values ('s101', 'john', 'bangalore', 'm', 50000);
+insert into employee13 values ('s101', 'john', 'bangalore', 'm', 50000);
 
-insert into employee22 values ('s102', 'januscott', 'dubai', 'f', 45000);
+insert into employee13 values ('s102', 'januscott', 'dubai', 'f', 45000);
 
-insert into employee22 values ('s103', 'kev scott', 'mumbai', 'f', 65000);
+insert into employee13 values ('s103', 'kev scott', 'mumbai', 'f', 65000);
 
-insert into employee22 values ('s104', 'suresh', 'hyderabad', 'm', 20000);
+insert into employee13 values ('s104', 'suresh', 'hyderabad', 'm', 20000);
 
-insert into employee22 values ('s105', 'rakesh', 'mumbai', 'm', 64000);
+insert into employee13 values ('s105', 'rakesh', 'mumbai', 'm', 64000);
