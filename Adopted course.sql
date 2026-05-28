482eb2852c138036d86d123390796cfe4c2ddb08
Mayank1094 <mayankkamble1094@gmail.com>
2026-05-28 10:04:06 +0530

    Clean up insert statements in SQL script
    Removed redundant insert statements from multiple tables.

--- Adopted course.sql
+++ Adopted course.sql
@@ -3,13 +3,9 @@ create table student13 (regno varchar2(10) primary key, name varchar2(15), major
 desc student13;
 
 insert into student13 values ('s001', 'aradhya', 'chemistry', '12-jan-04');
-
 insert into student13 values ('s002', 'anun', 'physics', '16-jan-02');
-
 insert into student13 values ('s003', 'kavya', 'cs', '23-apr-03');
-
 insert into student13 values ('s004', 'nitin', 'maths', '21-dec-04');
-
 insert into student13 values ('s005', 'radhika', 'account', '16-oct-03');
 
 select * from student13;
@@ -19,13 +15,9 @@ create table course13 (course int primary key, cname varchar2(15), dept varchar2
 desc course13;
 
 insert into course13 values (1, 'bsc', 'science');
-
 insert into course13 values (2, 'bcom', 'commerce');
-
 insert into course13 values (3, 'bca', 'cs');
-
 insert into course13 values (4, 'diploma', 'mechanical');
-
 insert into course13 values (5, 'be', 'engineer');
 
 select * from course13;
@@ -35,13 +27,9 @@ create table enrol13 (regno varchar2(10) references student13(regno), course int
 desc enrol13;
 
 insert into enrol13 values ('s001', 2, 3, 96);
-
 insert into enrol13 values ('s002', 4, 1, 78);
-
 insert into enrol13 values ('s003', 3, 2, 70);
-
 insert into enrol13 values ('s004', 1, 4, 93);
-
 insert into enrol13 values ('s005', 5, 6, 63);
 
 select * from enrol13;
@@ -51,15 +39,10 @@ create table text13 (book_isbn int primary key, book_title varchar2(15), publish
 desc text13;
 
 insert into text13 values (29, 'power', 'akash', 'bhavana');
-
 insert into text13 values (31, 'trignometry', 'manu', 'pratvi');
-
 insert into text13 values (15, 'java', 'asha', 'prasanth');
-
 insert into text13 values (16, 'python', 'sanvi', 'prasad');
-
 insert into text13 values (21, 'chemi', 'keerti', 'avani');
-
 insert into text13 values (17, 'php', 'asha', 'karan');
 
 select * from text13;
@@ -69,15 +52,10 @@ create table bookadoption13 (course int references course13(course), sem int, bo
 desc bookadoption13;
 
 insert into bookadoption13 values (3, 3, 15);
-
 insert into bookadoption13 values (3, 2, 16);
-
 insert into bookadoption13 values (3, 1, 31);
-
 insert into bookadoption13 values (3, 3, 21);
-
 insert into bookadoption13 values (2, 1, 29);
-
 insert into bookadoption13 values (3, 4, 17);
 
 select * from bookadoption13;
